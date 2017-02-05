0936368304da0873d5307717ff9faef6d2db031b
Juho Jaakkola <juho.a.jaakkola@gmail.com>
2017-02-05 21:15:37 +0000

    Prevents fatal error when triggering site upgrade
    The upgrade lib was trying to re-register an upgrade that already
    exists, which caused a fatal error.

--- lib/upgrades.php
+++ lib/upgrades.php
@@ -2,16 +2,20 @@
 
 use hypeJunction\MapsOpen\Geocoder;
 
-$count = Geocoder::getEntitiesWithoutGeocodes(['count' => true]);
-
 // Register upgrade scripts
 $path = 'admin/upgrades/maps/geocode';
 $upgrade = new ElggUpgrade();
 $upgrade = $upgrade->getUpgradeFromPath($path);
+
+if ($upgrade instanceof ElggUpgrade) {
+	// Upgrade already exists
+	return;
+}
+
+$count = Geocoder::getEntitiesWithoutGeocodes(['count' => true]);
+
 if ($count) {
-	if (!$upgrade instanceof ElggUpgrade) {
-		$upgrade = new ElggUpgrade();
-	}
+	$upgrade = new ElggUpgrade();
 	$upgrade->setPath($path);
 	$upgrade->title = elgg_echo('admin:upgrades:maps:geocode');
 	$upgrade->description = elgg_echo('admin:upgrades:maps:geocode:description');
